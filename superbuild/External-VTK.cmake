8a30a7dfb62c9dbf12f8a8201bc952891975e334
Laura PASCAL <laurajulia.pascal@gmail.com>
2016-05-20 10:27:36 -0400

    ENH: Adding some missing modules to VTK

--- superbuild/External-VTK.cmake
+++ superbuild/External-VTK.cmake
@@ -23,10 +23,12 @@ if( APPLE )
   set( _vtkOptions -DVTK_REQUIRED_OBJCXX_FLAGS:STRING="" )
 endif()
 
+set(Slicer_VTK_RENDERING_BACKEND "OpenGL")
+
 ExternalProject_Add(VTK
   DEPENDS ${VTK_DEPENDENCIES}
   GIT_REPOSITORY ${git_protocol}://vtk.org/VTK.git
-  GIT_TAG v6.1.0
+  GIT_TAG v6.3.0
   SOURCE_DIR VTK
   BINARY_DIR VTK-build
   UPDATE_COMMAND ""
@@ -43,9 +45,19 @@ ExternalProject_Add(VTK
     -DVTK_BUILD_ALL_MODULES:BOOL=OFF
     -DVTK_USE_SYSTEM_HDF5:BOOL=ON
     -DHDF5_DIR:PATH=${HDF5_DIR}
+    -DModule_vtkGUISupportQtOpenGL:BOOL=ON
+    -DModule_vtkGUISupportQtWebkit:BOOL=ON
+    -DModule_vtkGUISupportQtSQL:BOOL=ON
+    -DModule_vtkRenderingQt:BOOL=ON
+    -DVTK_USE_QVTK_QTOPENGL:BOOL=ON
+    -DModule_vtkTestingRendering:BOOL=ON
+    -DModule_vtkViewsQt:BOOL=ON
+    -DVTK_WRAP_PYTHON:BOOL=ON
+    -DModule_vtkWrappingPythonCore:BOOL=ON
+    -DVTK_RENDERING_BACKEND:STRING=${Slicer_VTK_RENDERING_BACKEND}
     -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DEPENDENCIES_DIR}
     -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
     -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
 )
 
-set( VTK_DIR ${INSTALL_DEPENDENCIES_DIR}/lib/cmake/vtk-6.1/ )
+set( VTK_DIR ${INSTALL_DEPENDENCIES_DIR}/lib/cmake/vtk-6.3/ )
